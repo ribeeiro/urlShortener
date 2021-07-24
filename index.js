a8c1fac55e2646a3dbcb567a755a61cde8409676
Thiago <thiagogr71@gmail.com>
2021-07-24 10:38:50 -0300

    remove body-parser(deprecated)

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ connection.authenticate().then(()=>{
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(session({
